2f6d15547c265efd4441a31cd76036a246898e94
FxIvan <almendraivan210814@gmail.com>
2024-04-08 19:52:11 -0300

    filtro

--- frontend/app/components/Admin/index.js
+++ frontend/app/components/Admin/index.js
@@ -15,7 +15,7 @@ const columns = [
 
 export default function PanelAdmin({ session, dataTable }) {
   const [dataTableReset, setDataTableReset] = useState(dataTable);
-  const data = useMemo(() => dataTableReset, []);
+  const data = useMemo(() => dataTableReset, [dataTableReset]);
   const [form, setForm] = useState({
     destination: "",
     typePayment: "",
@@ -59,52 +59,158 @@ export default function PanelAdmin({ session, dataTable }) {
       });
   };
 
+  const filterSubmit = async (dataFilter, type) => {
+    let paramFilter = "";
+
+    switch (type) {
+      case "date":
+        paramFilter += `?dateFilter=${dataFilter}`;
+        break;
+      case "amount":
+        paramFilter += `?amount=${dataFilter}`;
+        break;
+      case "typePayment":
+        paramFilter += `?typePayment=${dataFilter}`;
+        break;
+      default:
+        break;
+    }
+
+    await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/payment/filter${paramFilter}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setDataTableReset(data);
+      });
+  };
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-row">
-        <div className="my-8 w-1/2">
-          <div className="my-4">
-            <CSVLink
-              className="bg-black hover:bg-black/40 text-white font-bold py-2 px-4 rounded-full"
-              filename="my-file.csv"
-              data={csvData}
-            >
-              Export to CSV
-            </CSVLink>
+        <div className="my-8 w-4/6">
+          <div>
+            <div className="flex flex-row w-full justify-between">
+              <ul>
+                <li>Filtrar por fecha</li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("lessNow", "date");
+                  }}
+                >
+                  Mas nuevo
+                </li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("moreNow", "date");
+                  }}
+                >
+                  Mas viejo
+                </li>
+              </ul>
+              <ul>
+                <li>Filtrar por monto</li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("less", "amount");
+                  }}
+                >
+                  Menor a mayor
+                </li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("more", "amount");
+                  }}
+                >
+                  Mayor a menor
+                </li>
+              </ul>
+              <ul>
+                <li>Filtrar por tipo de pago</li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("debit", "typePayment");
+                  }}
+                >
+                  Debito
+                </li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("credit", "typePayment");
+                  }}
+                >
+                  Credito
+                </li>
+                <li
+                  className="cursor-pointer text-sm mt-2"
+                  onClick={() => {
+                    filterSubmit("cash", "typePayment");
+                  }}
+                >
+                  Efectivo
+                </li>
+              </ul>
+            </div>
+            <div></div>
+            <div></div>
           </div>
           <div>
-            <div className="App">
-              <table {...getTableProps()}>
-                <thead>
-                  {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                      {headerGroup.headers.map((column) => (
-                        <th {...column.getHeaderProps()}>
-                          {column.render("Header")}
-                        </th>
-                      ))}
-                    </tr>
-                  ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                  {rows.map((row) => {
-                    prepareRow(row);
-                    return (
-                      <tr {...row.getRowProps()}>
-                        {row.cells.map((cell) => (
-                          <td {...cell.getCellProps()}>
-                            {cell.render("Cell")}
-                          </td>
+            <div className="my-4">
+              <CSVLink
+                className="bg-black hover:bg-black/40 text-white font-bold py-2 px-4 rounded-full"
+                filename="my-file.csv"
+                data={csvData}
+              >
+                Export to CSV
+              </CSVLink>
+            </div>
+            <div>
+              <div className="App">
+                <table {...getTableProps()}>
+                  <thead>
+                    {headerGroups.map((headerGroup) => (
+                      <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                          <th {...column.getHeaderProps()}>
+                            {column.render("Header")}
+                          </th>
                         ))}
                       </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+                    ))}
+                  </thead>
+                  <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                      prepareRow(row);
+                      return (
+                        <tr {...row.getRowProps()}>
+                          {row.cells.map((cell) => (
+                            <td {...cell.getCellProps()}>
+                              {cell.render("Cell")}
+                            </td>
+                          ))}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-        <div className="my-8 w-1/2 ml-4 flex justify-center">
+        <div className="my-8 w-2/6 ml-4 flex justify-center">
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col my-4">
               <label htmlFor="destination">Destino</label>
@@ -124,11 +230,11 @@ export default function PanelAdmin({ session, dataTable }) {
                 id="typePayment"
                 value={form.typePayment}
                 onChange={handleChange}
-                className="mt-4"
+                className="mt-4 border border-gray-300 rounded-md"
               >
-                <option value="efectivo">Efectivo</option>
-                <option value="tarjeta">Tarjeta</option>
-                <option value="transferencia">Transferencia</option>
+                <option value="debit">Debito</option>
+                <option value="credit">Credito</option>
+                <option value="cash">Efectivo</option>
               </select>
             </div>
             <div className="flex flex-col my-4">
