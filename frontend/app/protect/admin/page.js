8a02d0aaf64fa9dbc0e35613cf319d8148b34d59
FxIvan <almendraivan210814@gmail.com>
2024-04-08 20:20:59 -0300

    protect route

--- frontend/app/protect/admin/page.js
+++ frontend/app/protect/admin/page.js
@@ -1,9 +1,14 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 import PanelAdmin from "components/app/components/Admin";
+import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
 const getListRegister = async () => {
   const session = await getServerSession(authOptions);
+  if (!session) {
+    return redirect("/auth/login");
+  }
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/payment/list`,
     {
